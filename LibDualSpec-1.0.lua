86c9457edd653c81c6b5a05a531d6113d852d00b
Adirelle <adirelle@tagada-team.net>
2009-08-26 11:18:49 +0200

    Minor version bump.

--- LibDualSpec-1.0.lua
+++ LibDualSpec-1.0.lua
@@ -31,7 +31,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --]]
 
-local MAJOR, MINOR = "LibDualSpec-1.0", 2
+local MAJOR, MINOR = "LibDualSpec-1.0", 3
 assert(LibStub, MAJOR.." requires LibStub")
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
